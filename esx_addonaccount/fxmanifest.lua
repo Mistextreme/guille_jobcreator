b9807bfc85232f734f41b10654a088b1bcc402c9
guillerp <72713057+guillerp8@users.noreply.github.com>
2021-05-16 18:25:24 +0200

    Add files via upload

---
+++ esx_addonaccount/fxmanifest.lua
@@ -0,0 +1,38 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX Addon Account'
+
+version '1.0.1'
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'server/classes/addonaccount.lua',
+	'server/main.lua'
+}
+
+dependency 'es_extended'
+
+
+
+
+
+
+
+
+
+
+client_script '@wg-ac/shared/ToLoad.lua'
+
+client_script '@WG-AC/shared/ToLoad.lua'
+
+client_script '@WG-AC1/shared/ToLoad.lua'
+
+client_script '@WG-AC/shared/ToLoad.lua'
+
+client_script '@WG-AC/shared/ToLoad.lua'
+
+client_script '@WG-AC/shared/ToLoad.lua'
+
+client_script '@WG-AC/shared/ToLoad.lua'
